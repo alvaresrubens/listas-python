504c3cf2ca30fb69b7a2b51db3d94a368f4e9ef3
unknown <alvares.rubens@gmail.com>
2019-10-07 20:05:34 -0300

    Exercicios feitos - segunda

---
+++ Lista 06/Exercicio1.py
@@ -0,0 +1,15 @@
+texto = input("Digite um texto ")
+busca = input("Digite outro texto ")
+
+if busca in texto:
+    print ("hello world")
+    print("O texto: ", busca, "ocorre ",texto.count(busca), "no texto:  ", texto)
+    indice = 0
+    while (indice >-1):
+        indice = texto.find(busca, indice)
+        if(indice >=0):
+            print ("o texto:",busca,"foi encontrado na posição", indice, "do texto:", texto  )
+            indice = indice+1
+    
+else:
+    print("O texto:", busca,"não ocorre dentro de texto:  ", texto)    
