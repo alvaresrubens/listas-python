d9ac20e6b9948c0bbd2315e53a2fbac6b69cad3d
Rubens-Gaia <alvares.rubens@gmail.com>
2019-10-29 10:16:39 -0200

    Dia 29-10

---
+++ Lista 07/Exercicio1.py
@@ -0,0 +1,11 @@
+lista = [55,39,12,25,95,2]
+contador = 0
+
+for numero in lista:
+    contador = contador + numero
+
+media = contador/len(lista)
+print (media)
+
+for numero in lista:
+    numero - media
